7a733198059accdc16889d0730f42694be729860
Etienne Charlier <etienne@charlier.info>
2018-12-09 14:03:05 +0100

    Nettoyage

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 # Déplacement du plist :
 sudo cp com.nicolinux.backupEFI.plist /System/Library/LaunchDaemons/
+# Positionnement des permissions
 sudo chown root /System/Library/LaunchDaemons/com.nicolinux.backupEFI.plist
 sudo chgrp wheel /System/Library/LaunchDaemons/com.nicolinux.backupEFI.plist
+# Activation
 sudo launchctl load -w /System/Library/LaunchDaemons/com.nicolinux.backupEFI.plist
-
